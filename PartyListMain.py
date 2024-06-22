e9650601acdc8f112b9655c31e4927ecdd8ceea1
Hanka <hrosicka@post.cz>
2024-06-22 10:59:31 +0200

    Styles and design improvement

--- PartyListMain.py
+++ PartyListMain.py
@@ -63,7 +63,7 @@ class PartyWindow(QMainWindow):
 
         self.table_party_list.setModel(self.personModel.model)
         self.table_party_list.setSelectionBehavior(QAbstractItemView.SelectRows) 
-        self.table_party_list.horizontalHeader().setStyleSheet('QHeaderView::section {background-color: rgb(69, 86, 206); color: rgb(250, 250, 250); font-weight: bold;} QToolTip { background-color: #8ad4ff; color: black; border: #8ad4ff solid 1px}')
+        self.table_party_list.horizontalHeader().setStyleSheet('QHeaderView::section {background-color: #4562C3; color: rgb(250, 250, 250); font-weight: bold;} QToolTip { background-color: #8ad4ff; color: black; border: #8ad4ff solid 1px}')
         self.table_party_list.verticalHeader().setStyleSheet('QHeaderView::section {background-color: rgb(60, 60, 60); color: rgb(200, 250, 200); font-size: 6} QToolTip { background-color: #8ad4ff; color: black; border: #8ad4ff solid 1px}')
 
         self.button_new.clicked.connect(self.openAddPerson)  
